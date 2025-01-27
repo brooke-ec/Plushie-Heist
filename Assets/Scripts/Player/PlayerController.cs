91a99b22eaf190e95f058b2466d21d03ed690b79
Jess Williamson <bmw28@sussex.ac.uk>
2025-01-27 14:56:51 +0000

    Created the character controller and can look and rotate

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[SelectionBase]
+public class PlayerController : MonoBehaviour
+{
+    #region Serialized fields
+    /// <summary> Maximum player Speed</summary>
+    [SerializeField] private float maxSpeed;
+    /// <summary> Sensitivity of the mouse </summary>
+    [SerializeField] private float lookSensitivity;
+    /// <summary>Maximum pitch of the camera</summary>
+    [SerializeField] private float MaxPitch;
+
+    #endregion
+
+    #region private fields
+    /// <summary>Current Player Velocity (NOT the character controller velocity we dont like that one)</summary>
+    private Vector3 velocity;
+    /// <summary> Character controller asset</summary>
+    private CharacterController cc;
+    /// <summary> WASD Input  value </summary>
+    private Vector2 wasdInput;
+    /// <summary> Mouse Input Value </summary>
+    private Vector2 lookInput;
+    /// <summary> Camera Asset attached to player </summary>
+    private Camera cam;
+    /// <summary> current pitch of the camera </summary>
+    private float camPitch;
+    #endregion
+
+    #region core methods
+    public void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        cam = GetComponentInChildren<Camera>();
+        
+    }
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+        LookandRotate();
+    }
+    #endregion
+
+
+    #region private Methods
+    /// <summary>
+    /// Called every frame to update where the camera looks and to rotate the character
+    /// </summary>
+    private void LookandRotate()
+    {
+        transform.Rotate(new Vector3(0, lookInput.x, 0));
+
+        camPitch = Mathf.Clamp(camPitch+lookInput.y, -MaxPitch, MaxPitch);
+
+        cam.transform.localEulerAngles = new Vector3(-camPitch, 0, 0);
+        
+    }
+    #endregion
+    #region Input
+
+    public void GetMoveInput(InputAction.CallbackContext ctx)
+    {
+        wasdInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void GetLookInput(InputAction.CallbackContext ctx)
+    {
+        Vector2 input = ctx.ReadValue<Vector2>();
+        lookInput = input * lookSensitivity;
+    }
+
+    #endregion
+}
