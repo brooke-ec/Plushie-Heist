bf4f7616067ca00732687c99699b68ce0348cb53
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-02-06 20:54:34 +0000

    feat: recording list

---
+++ PlaytestDashboard/src/routes/dashboard/+page.server.ts
@@ -0,0 +1,18 @@
+import { UPLOADS_ROOT } from "$lib";
+import fs from "fs/promises";
+import path from "path";
+
+export async function load() {
+	const files = await fs.readdir(UPLOADS_ROOT);
+
+	const uploads = await Promise.all(
+		files.map(async (file) => {
+			const id = file.replace(/\.[^/.]+$/, "");
+			const stat = await fs.stat(path.join(UPLOADS_ROOT, file));
+
+			return { id, file, date: stat.ctime };
+		}),
+	);
+
+	return { uploads };
+}
