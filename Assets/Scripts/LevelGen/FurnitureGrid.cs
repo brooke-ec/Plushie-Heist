6eddc304cb7d678b32c70910a921ae0c68f681fa
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-03-23 21:32:20 +0000

    feat: improve gizmo

--- Assets/Scripts/LevelGen/FurnitureGrid.cs
+++ Assets/Scripts/LevelGen/FurnitureGrid.cs
@@ -9,25 +9,37 @@ public class FurnitureGrid : MonoBehaviour
     [SerializeField] private float cellSize = 2;
     [SerializeField] private float spacing = 0.25f;
 
-    private Vector2Int size { get { return new Vector2Int((int)(collider.size.x / cellSize), (int)(collider.size.z / cellSize)); } }
-    private float height { get { return collider.bounds.max.y; } }
+    private Vector2Int size => new Vector2Int((int)(collider.size.x / cellSize), (int)(collider.size.z / cellSize));
+    private float height => collider.center.y + collider.size.y / 2;
 
     new private BoxCollider collider;
 
+#if UNITY_EDITOR
+    private Vector3 oldSize = Vector3.zero;
+    private Mesh mesh;
+
     private void OnDrawGizmos()
     {
+        if (collider.size != oldSize || mesh == null) mesh = BuildMarker();
+
         Gizmos.color = Color.blue;
-        collider = GetComponent<BoxCollider>();
-        Gizmos.DrawWireCube(transform.TransformPoint(collider.center), collider.size);
+        Gizmos.DrawWireMesh(mesh, transform.TransformPoint(collider.center));
     }
+
+    private void OnValidate()
+    {
+        if (collider == null) collider = GetComponent<BoxCollider>();
+        mesh = null;
+    }
+#endif
 
     private void Start()
     {
         collider = GetComponent<BoxCollider>();
-        GetComponent<MeshFilter>().mesh = Build();
+        GetComponent<MeshFilter>().mesh = BuildMarker();
     }
 
-    private Mesh Build()
+    private Mesh BuildMarker()
     {
         List<Vector3> verticies = new List<Vector3>();
         List<int> indicies = new List<int>();
@@ -54,6 +66,7 @@ public class FurnitureGrid : MonoBehaviour
         Mesh mesh = new Mesh();
         mesh.SetVertices(verticies);
         mesh.SetIndices(indicies, MeshTopology.Triangles, 0);
+        mesh.SetNormals(verticies.Select(_ => new Vector3(0, 1, 0)).ToArray());
         return mesh;
     }
 }
