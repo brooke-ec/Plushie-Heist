3b17f498c88ecd26dec44a661255fac0b591ad5a
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-03-24 00:14:45 +0000

    feat: hide grid when not placing

--- Assets/Scripts/LevelGen/FurnitureGrid.cs
+++ Assets/Scripts/LevelGen/FurnitureGrid.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(BoxCollider))]
--- Assets/Scripts/LevelGen/FurniturePlacer.cs
+++ Assets/Scripts/LevelGen/FurniturePlacer.cs
@@ -3,11 +3,46 @@ using UnityEngine.InputSystem;
 
 public class FurniturePlacer : MonoBehaviour
 {
-    [SerializeField] float maxDistance = 10;
-    [SerializeField] FurnitureItem[] test;
 
+    [SerializeField] private float maxDistance = 10;
+    
     private FurnitureItem item;
+    private new Camera camera;
+    private int mask;
+
+    [Header("Testing")]
+    [SerializeField] FurnitureItem[] test;
     private int testIndex = 0;
+
+    private void Start()
+    {
+        mask = LayerMask.GetMask("Furniture Grid");
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (item != null)
+        {
+            camera.cullingMask = camera.cullingMask | mask;
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, mask))
+            {
+                FurnitureGrid grid = hit.transform.GetComponent<FurnitureGrid>();
+
+                Vector2 coordinates = grid.FromWorldspace(hit.point);
+                Region region = Region.FromSize(
+                    Mathf.FloorToInt(coordinates.x),
+                    Mathf.FloorToInt(coordinates.y),
+                    item.size.x, item.size.y
+                );
+
+                item.transform.position = grid.ToWorldspace(region.center);
+            }
+        }
+        else camera.cullingMask = camera.cullingMask & ~mask;
+    }
 
     public void OnPlace(InputAction.CallbackContext ctx)
     {
@@ -24,26 +59,5 @@ public class FurniturePlacer : MonoBehaviour
     {
         if (ctx.ReadValueAsButton() || item == null) return;
         item.Rotate();
-    }
-
-    private void Update()
-    {
-        if (item != null)
-        {
-            Ray ray = new Ray(transform.position, transform.forward);
-            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, LayerMask.GetMask("Furniture Grid")))
-            {
-                FurnitureGrid grid = hit.transform.GetComponent<FurnitureGrid>();
-
-                Vector2 coordinates = grid.FromWorldspace(hit.point);
-                Region region = Region.FromSize(
-                    Mathf.FloorToInt(coordinates.x),
-                    Mathf.FloorToInt(coordinates.y),
-                    item.size.x, item.size.y
-                );
-
-                item.transform.position = grid.ToWorldspace(region.center);
-            }
-        }
     }
 }
