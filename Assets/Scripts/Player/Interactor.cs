d93871cbbee02db158b6a110146a8f4a99d98252
Jessie Williamson <bmwilliamson4@googlemail.com>
2025-04-14 17:26:30 +0100

    interactor script, interactable interface and pickup script added, pickup script unfinished

---
+++ Assets/Scripts/Player/Interactor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform interactorPoint;
+    [SerializeField] private float interactorRadius;
+    [SerializeField] private LayerMask interactorLayerMask;
+
+    private readonly Collider[] colliders = new Collider[1];
+    [SerializeField] private int numColliders;
+
+    private void Update()
+    {
+        numColliders = Physics.OverlapSphereNonAlloc(interactorPoint.position, interactorRadius, colliders, interactorLayerMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(interactorPoint.position, interactorRadius);
+    }
+
+    public void pressInteract(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            if (numColliders > 0)
+            {
+                IInteractable interactable = colliders[0].GetComponent<IInteractable>();
+                if (interactable != null) { interactable.interact(this); }
+            }
+        }
+    }
+}
