943c17dddbc24ac3a68595d3200c78de70fcb6a4
Jessie Williamson <bmwilliamson4@googlemail.com>
2025-04-14 21:02:36 +0100

    comments

--- Assets/Scripts/Player/Interactor.cs
+++ Assets/Scripts/Player/Interactor.cs
@@ -13,18 +13,24 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform interactorPoint;
     /// <summary>The radius of the interactor from the point</summary>
     [SerializeField] private float interactorRadius;
+    /// <summary>Layer to interact with should be Interactable add others if needed(shouldnt be) </summary>
     [SerializeField] private LayerMask interactorLayerMask;
-    [SerializeField] private int numColliders;
+    /// <summary>The UI element to show the Interaction Text </summary>
     [SerializeField] private TextMeshProUGUI interactionText;
 
+    /// <summary>number of colliders currently interactable with</summary>
+    private int numColliders;
+    /// <summary>array to hold colliders that are currently in range</summary>
     private readonly Collider[] colliders = new Collider[1];
+    /// <summary>is Ui Text displayed </summary>
     private bool textDisplayed = false;
 
     private void Update()
     {
+        //get interactables in range
         numColliders = Physics.OverlapSphereNonAlloc(interactorPoint.position, interactorRadius, colliders, interactorLayerMask);
 
-        if (numColliders > 0 && !textDisplayed)
+        if (numColliders > 0 && !textDisplayed) // show relevant text 
         {
             if (colliders[0] != null) 
             {
@@ -33,19 +39,23 @@ public class Interactor : MonoBehaviour
             interactionText.gameObject.SetActive(true);
             textDisplayed = true;
         }
-        else if(numColliders ==0 && textDisplayed) 
+        else if(numColliders ==0 && textDisplayed) // hide text
         {
             interactionText.gameObject.SetActive(false);
             textDisplayed = false;
         }
     }
 
+    // For Debugging
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(interactorPoint.position, interactorRadius);
     }
-
+    /// <summary>
+    /// If Interactor is close enough to interact call the interactables interact Method when interact button pressed
+    /// </summary>
+    /// <param name="ctx"></param>
     public void pressInteract(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
