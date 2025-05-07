b87820587649dcb0533aadc13379cddd689472e0
Veciri <veronicaolivo555@gmail.com>
2025-05-07 14:43:57 +0100

    Main menu done

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -1,9 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject saveFileIconPrefab;
+    public Transform saveFileContentTransform;
+    public GameObject noSavedGamesText;
+
+    private void Start()
+    {
+        MakeSavesAsUI();
+    }
+    public void MakeSavesAsUI()
+    {
+        //LOAD LIST OF SAVE FILES
+
+        /*
+        List<SaveFile?> saveFiles = new List<SaveFile>();
+        for(int i=0;i<saveFiles.Count; i++)
+        {
+            GameObject savefileUI = Instantiate(saveFileIconPrefab, saveFileContentTransform);
+            savefileUI.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>().text = saveFiles[i].totalTimePlayed;
+
+            savefileUI.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => LoadSave(saveFiles[i]));
+        }
+
+        noSavedGamesText.SetActive(saveFiles.Count == 0);
+        */
+    }
+
+    /*
+    public void LoadSave(SaveFile? saveFile)
+    {
+        //TO-DO
+    }*/
+
+    public void NewGame()
+    {
+        //TO-DO
+    }
+
     public void CloseGame()
     {
         Application.Quit();
