c851c988f2ed4130b7439669ee7a3e62da03c5bf
Veciri <veronicaolivo555@gmail.com>
2025-05-07 15:48:23 +0100

    Added spinning plushie

--- Assets/Scripts/UI/LoadingScreen/LoadingSceneController.cs
+++ Assets/Scripts/UI/LoadingScreen/LoadingSceneController.cs
@@ -13,7 +13,11 @@ public class LoadingSceneController : MonoBehaviour
     [SerializeField] private GameObject loadingScreenCanvasPrefab;
     private GameObject loadingScreenCanvas;
 
+    private TextMeshProUGUI loadingText;
     private Slider progressBar;
+    private Image plushie;
+
+    [SerializeField] private List<Sprite> plushieIcons;
 
     private void Awake()
     {
@@ -27,7 +31,10 @@ public class LoadingSceneController : MonoBehaviour
         loadingScreenCanvas = Instantiate(loadingScreenCanvasPrefab);
         progressBar = loadingScreenCanvas.transform.GetChild(1).GetComponent<Slider>();
         loadingText = loadingScreenCanvas.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+        plushie = loadingScreenCanvas.transform.GetChild(4).GetComponent<Image>();
+
         StartCoroutine(UpdateLoadingText("Loading Scene"));
+        StartCoroutine(SpinPlushie());
 
         sceneLoading = SceneManager.LoadSceneAsync(sceneString);
         StartCoroutine(GetSceneLoadingProgress());
@@ -43,13 +50,25 @@ public class LoadingSceneController : MonoBehaviour
         Destroy(loadingScreenCanvas);
     }
 
-    private TextMeshProUGUI loadingText;
-
     public IEnumerator UpdateLoadingText(string text)
     {
         loadingText.GetComponent<CanvasGroup>().DOFade(0, 0.5f);
         yield return new WaitForSeconds(0.5f);
         loadingText.text = text;
         loadingText.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+    }
+
+    public IEnumerator SpinPlushie()
+    {
+        int val = Random.Range(0, plushieIcons.Count);
+        plushie.sprite = plushieIcons[val];
+
+        float spinSpeed = 100f;
+        while (loadingScreenCanvas!=null)
+        {
+            plushie.transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+            yield return null;
+        }
+        
     }
 }
