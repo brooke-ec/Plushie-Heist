3933025a00616a814a0cc7e80ab95aa1ec3e5899
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-05-10 17:59:19 +0100

    feat: LODs

--- LODGenerator/app.py
+++ LODGenerator/app.py
@@ -1,16 +1,22 @@
 import os
 import subprocess
+import time
 from tkinter import filedialog
+
+MAX_PROCESSES = 1
 
 workbench = os.path.join(os.path.dirname(__file__), "workbench.blend")
 script = os.path.join(os.path.dirname(__file__), "script.py")
-directory = filedialog.askdirectory()
+files = filedialog.askopenfilenames(filetypes=(("FBX", ".fbx"),))
 
-processes = []
-for file in os.listdir(directory):
+processes: list[subprocess.Popen] = []
+for file in files:
     if file.lower().endswith(".fbx"):
-        path = os.path.join(directory, file)
-        processes.append(subprocess.Popen(["blender", "-b", workbench, "-P", script, "--", os.path.abspath(path)]))
+        processes.append(subprocess.Popen(["blender", "-b", workbench, "-P", script, "--", os.path.abspath(file)]))
 
-[p.wait() for p in processes]
+    while len(processes) >= MAX_PROCESSES:
+        processes = [p for p in processes if p.poll is None]
+        time.sleep(0.1)
+
 print("All files processed!")
+input()
