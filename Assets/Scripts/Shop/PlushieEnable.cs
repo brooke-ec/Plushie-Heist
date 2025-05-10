7f1899b0aee3a634805a48bce020b6374b5d850b
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-05-10 23:52:03 +0100

    fix: boss collider player collisions

--- Assets/Scripts/Shop/PlushieEnable.cs
+++ Assets/Scripts/Shop/PlushieEnable.cs
@@ -1,23 +1,12 @@
-using cakeslice;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlushieEnable : MonoBehaviour, IInteractable
+public class PlushieEnable : MonoBehaviour
 {
-    [SerializeField] PlushieInfo plushie;
+    [SerializeField] public PlushieInfo plushie;
     [SerializeField] bool inverted = false;
-
-    string IInteractable.interactionPrompt => "Press F to Squish " + plushie.name;
 
     void Start()
     {
-        GetComponentsInChildren<Renderer>().ForEach(r => r.AddComponent<Outline>().enabled = false);
-        GetComponentsInChildren<Collider>().ForEach(c => c.gameObject.layer = LayerMask.NameToLayer("Interactable"));
         gameObject.SetActive(plushie.unlocked == !inverted);
-    }
-
-    void IInteractable.PrimaryInteract(Interactor interactor)
-    {
-        AudioManager.instance.PlaySound(AudioManager.SoundEnum.squeak);
     }
 }
---
+++ Assets/Scripts/Shop/PlushieSquish.cs
@@ -0,0 +1,19 @@
+﻿using cakeslice;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlushieSquish: PlushieEnable, IInteractable
+{
+    string IInteractable.interactionPrompt => "Press F to Squish " + plushie.name;
+
+    private void Start()
+    {
+        GetComponentsInChildren<Renderer>().ForEach(r => r.AddComponent<Outline>().enabled = false);
+        GetComponentsInChildren<Collider>().ForEach(c => c.gameObject.layer = LayerMask.NameToLayer("Interactable"));
+    }
+
+    void IInteractable.PrimaryInteract(Interactor interactor)
+    {
+        AudioManager.instance.PlaySound(AudioManager.SoundEnum.squeak);
+    }
+}
